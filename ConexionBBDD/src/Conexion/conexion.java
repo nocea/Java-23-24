7064afe815dd004c6b5ecce0bf847f111f8c5277
Mario Nocea Cabrera <mnoceacabrera@gmail.com>
2023-09-27 11:58:53 +0200

    a

--- ConexionBBDD/src/Conexion/conexion.java
+++ ConexionBBDD/src/Conexion/conexion.java
@@ -6,6 +6,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 public class conexion {
+
 public static void main(String[] args)  {
 	//Instancio el objeto Properties
 	Properties propiedades=new Properties();
